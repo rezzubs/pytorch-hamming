2ce16d8ce5584ed30057cab8e9675276e59f4018
Marten Roots <rezzubs@proton.me>
2025-08-05 18:15:16 +0300

    Remove rendundant python modules

--- pytorch_ecc/__init__.py
+++ pytorch_ecc/__init__.py
@@ -1,5 +1,1 @@
 "Error correction utilities for pytorch"
-
-from ._hamming import hamming_encode, hamming_error_bit_index, hamming_decode
-
-__all__ = ["hamming_encode", "hamming_error_bit_index", "hamming_decode"]
