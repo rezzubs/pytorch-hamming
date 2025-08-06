67fa9d6f8f8109c354f0173c6b974fc9b03c3b81
Marten Roots <rezzubs@proton.me>
2025-08-06 16:51:32 +0300

    Add script for testing accuracy

---
+++ test.py
@@ -0,0 +1,58 @@
+import torch
+from torch import nn
+import torchvision
+from torchvision import transforms
+
+from pytorch_ecc._hamming import (
+    hamming_decode_module,
+    hamming_encode_module,
+)
+
+model: nn.Module = torch.hub.load(
+    "chenyaofo/pytorch-cifar-models", "cifar10_resnet20", pretrained=True
+)  # type: ignore
+
+
+mean = (0.49139968, 0.48215827, 0.44653124)
+std = (0.24703233, 0.24348505, 0.26158768)
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean, std)])
+testset = torchvision.datasets.CIFAR10(
+    root="./data", train=False, download=True, transform=transform
+)
+dataloader = torch.utils.data.DataLoader(
+    testset,
+    batch_size=8,
+    shuffle=False,
+)
+
+
+def evaluate(model: nn.Module):
+    global dataloader
+    model.eval()
+    num_samples = torch.tensor(0)
+    num_correct = torch.tensor(0)
+
+    for data in dataloader:
+        inputs, targets = data[0], data[1]
+
+        outputs = model(inputs)
+
+        # Convert logits to class indices
+        outputs = outputs.argmax(dim=1)
+
+        # Update metrics
+        num_samples += targets.size(0)
+        num_correct += (outputs == targets).sum()
+
+        # break
+
+    return (num_correct / num_samples * 100).item()
+
+
+print(evaluate(model))
+
+hamming_encode_module(model)
+hamming_decode_module(model)
+
+print(evaluate(model))
