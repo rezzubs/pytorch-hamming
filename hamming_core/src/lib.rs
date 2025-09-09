4e2393f780a7e107928ba68456f10c7d7deb2e9d
Marten Roots <rezzubs@proton.me>
2025-09-09 10:11:14 +0300

    Add rust impl for encoding/decoding 16 bit data

--- hamming_core/src/lib.rs
+++ hamming_core/src/lib.rs
@@ -93,5 +93,70 @@ mod hamming_core {
 
             Ok((PyArray1::from_slice(py, &output), failed_decodings))
         }
+
+        /// Encode an array of uint16 values as an array of uint8 values.
+        ///
+        /// This is used as a placeholder for f16 encoding because f16 is unstable in rust.
+        ///
+        /// See module docs for details.
+        #[pyfunction]
+        fn encode_u16<'py>(
+            py: Python<'py>,
+            input: PyReadonlyArray1<'py, u16>,
+        ) -> Bound<'py, PyArray1<u8>> {
+            let mut input = input.as_array().into_iter().copied().collect::<Vec<u16>>();
+
+            let required_padding = input.len() % 4;
+            input.extend(std::iter::repeat_n(0, required_padding));
+
+            PyArray1::from_iter(
+                py,
+                input
+                    .into_iter()
+                    .tuples()
+                    .flat_map(|(a, b, c, d)| Hamming64::encode([a, b, c, d]).0.into_bytes()),
+            )
+        }
+        /// Decode an array of uint8 values into an array of float32 values.
+        ///
+        /// Returns: (decoded_array, num_unmasked_faults)
+        #[pyfunction]
+        fn decode_u16<'py>(
+            py: Python<'py>,
+            input: PyReadonlyArray1<'py, u8>,
+        ) -> PyResult<(Bound<'py, PyArray1<u16>>, u64)> {
+            let input = input.as_array();
+
+            if input.len() % Hamming64::NUM_BYTES != 0 {
+                return Err(PyValueError::new_err(format!(
+                    "Expected a number of bytes divisible by {}",
+                    Hamming64::NUM_BYTES
+                )));
+            }
+
+            let mut iter = input.iter().copied();
+            let num_encoded = input.len() / Hamming64::NUM_BYTES;
+            let mut output = Vec::with_capacity(num_encoded * 2);
+
+            let mut failed_decodings: u64 = 0;
+
+            for _ in 0..num_encoded {
+                let bytes: [u8; Hamming64::NUM_BYTES] = iter.next_array().expect("Within bounds");
+
+                let encoded = Hamming64(bytes.into());
+                let (decoded, success) = encoded.decode();
+
+                if !success {
+                    failed_decodings = failed_decodings
+                        .checked_add(1)
+                        .expect("Unexpectedly large number of unmasked faults");
+                }
+
+                let [a, b, c, d]: [u16; 4] = decoded.into();
+                output.extend([a, b, c, d]);
+            }
+
+            Ok((PyArray1::from_slice(py, &output), failed_decodings))
+        }
     }
 }
