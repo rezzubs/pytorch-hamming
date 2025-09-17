44ca95898d6b21c4450301a9a3b6a0e3263a3c71
Marten Roots <rezzubs@proton.me>
2025-09-17 20:47:18 +0300

    Add pytorch bindings for new bit-widths

--- src/hamming_utils/u128/_core.py
+++ src/hamming_utils/u128/_core.py
@@ -1,7 +1,6 @@
 from hamming_utils._common import decode_impl, encode_impl, tensor_list_fi_impl
 import torch
 import hamming_core
-import numpy as np
 
 
 # FIXME: Remove `type: ignore` comments after the hamming_core functions get
@@ -9,11 +8,11 @@ import numpy as np
 
 
 def encode_f32(tensor: torch.Tensor) -> torch.Tensor:
-    """Enocde a flattened float32 tensor as 9 byte hamming codes.
+    """Enocde a flattened float32 tensor as 18 byte hamming codes.
 
     See module docs for details.
     """
-    f = hamming_core.u64.encode_f32  # type: ignore
+    f = hamming_core.u128.encode_f32  # type: ignore
     return encode_impl(tensor, torch.float32, None, f)
 
 
@@ -22,16 +21,16 @@ def decode_f32(tensor: torch.Tensor) -> tuple[torch.Tensor, int]:
 
     See module docs for details.
     """
-    f = hamming_core.u64.decode_f32  # type: ignore
+    f = hamming_core.u128.decode_f32  # type: ignore
     return decode_impl(tensor, torch.float32, None, f)
 
 
 def encode_f16(tensor: torch.Tensor) -> torch.Tensor:
-    """Enocde a flattened float32 tensor as 9 byte hamming codes.
+    """Enocde a flattened float32 tensor as 18 byte hamming codes.
 
     See module docs for details.
     """
-    f = hamming_core.u64.encode_f16  # type: ignore
+    f = hamming_core.u128.encode_f16  # type: ignore
     return encode_impl(tensor, torch.float16, torch.uint16, f)
 
 
@@ -40,7 +39,7 @@ def decode_f16(tensor: torch.Tensor) -> tuple[torch.Tensor, int]:
 
     See module docs for details.
     """
-    f = hamming_core.u64.decode_f16  # type: ignore
+    f = hamming_core.u128.decode_f16  # type: ignore
     return decode_impl(tensor, torch.float16, torch.uint16, f)
 
 
@@ -53,7 +52,7 @@ def encoded_tensor_list_fi(
 
     See module docs for details.
     """
-    f = hamming_core.u64.array_list_fi  # type: ignore
+    f = hamming_core.u128.array_list_fi  # type: ignore
     tensor_list_fi_impl(
         tensors,
         bit_error_rate,
---
+++ src/hamming_utils/u32/__init__.py
@@ -0,0 +1,36 @@
+"""Functions for working with hamming codes with 32 data bits.
+
+The encoding functions add padding to the tensors to make the number of bits
+a multiple of 32. These extra values will not be used as targets for fault
+injection but cannot be removed before decoding.
+
+The padding needs to be removed manually after decoding because there's no
+way for the decoding function to know how many items were stored in the tensor
+initially there's no way for the decoding function to know how many items were
+stored in the tensor initially.
+
+The encoding and decoding functions do not touch the original tensors and return
+new ones instead. The fault injection function replaces the original tensor
+in place.
+
+The encoding functions will return a single dimensional tensor of bytes. The
+decoding functions expect the same format as input and also return flattened
+tensors because there's no way to know the original shape.
+tensors instead.
+"""
+
+from ._core import (
+    decode_f16,
+    decode_f32,
+    encode_f16,
+    encode_f32,
+    encoded_tensor_list_fi,
+)
+
+__all__ = [
+    "decode_f16",
+    "decode_f32",
+    "encode_f16",
+    "encode_f32",
+    "encoded_tensor_list_fi",
+]
