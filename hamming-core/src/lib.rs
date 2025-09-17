efa56b8a4c040a5dc3574cb7f08034fd18f557c8
Marten Roots <rezzubs@proton.me>
2025-09-17 14:11:52 +0300

    Update docs of python bindings

--- hamming-core/src/lib.rs
+++ hamming-core/src/lib.rs
@@ -7,21 +7,11 @@ pub use bit_buffer::{BitBuffer, SizedBitBuffer};
 pub use encoding::{Decodable, Encodable, Init};
 use pyo3::pymodule;
 
-/// Encoding and decoding floating point arrays with hamming codes.
-///
-/// Currently only chunks of 8 bytes (module u64) is supported.
 #[pymodule]
 mod hamming_core {
     use pyo3::pymodule;
 
-    /// Data will be encoded in groups of 8 bytes and the corresponding encoded version will equal 9
-    /// bytes. Any input array which isn't evenly divisible into 8 byte chunks will padded with extra
-    /// zeros that need to be removed manually after decoding.
-    ///
-    /// All encoding functions take floating point arrays as inputs and return uint8 arrays. The
-    /// supported input datatypes are float32 and float16.
-    ///
-    /// The decoding functions additionally return the number of detected faults.
+    /// Functions related to encoding 64 byte data
     #[pymodule]
     mod u64 {
         #[pymodule_export]
@@ -36,6 +26,7 @@ mod hamming_core {
         use crate::python::u64::encode_u16;
     }
 
+    /// Fault injection for generic arrays.
     #[pymodule]
     mod generic {
         #[pymodule_export]
