6272bac2c8c0bebe2ef73ad8c62df41f9bc4847e
Marten Roots <rezzubs@proton.me>
2025-10-01 12:06:09 +0300

    feat: Add new implementation of compare_module_bitwise on the rust side

--- hamming-core/src/python/generic.rs
+++ hamming-core/src/python/generic.rs
@@ -1,10 +1,10 @@
-//! Fault injection on generic arrays & lists of arrays.
+//! Functions for generic arrays & lists of arrays.
 use crate::{
     python::common::{fi_context_create, prep_input_array, prep_input_array_list},
     BitBuffer,
 };
 use numpy::PyArray1;
-use pyo3::prelude::*;
+use pyo3::{exceptions::PyValueError, prelude::*};
 
 use crate::python::common::{FiContext, InputArr, OutputArr};
 
@@ -82,4 +82,45 @@ pub fn u16_array_list_fi<'py>(
             .collect::<Vec<_>>(),
         fi_context_create(num_faults, num_bits),
     ))
+}
+
+/// Computes bit masks for non-matching bits for all items.
+#[pyfunction]
+pub fn compare_array_list_bitwise_f32<'py>(
+    _py: Python<'py>,
+    a: Vec<InputArr<f32>>,
+    b: Vec<InputArr<f32>>,
+) -> PyResult<Vec<u32>> {
+    if a.len() != b.len() {
+        return Err(PyValueError::new_err(
+            "The lengths of `a` and `b` don't match",
+        ));
+    }
+
+    let output = a
+        .into_iter()
+        .zip(b)
+        .map(|(a_inner, b_inner)| -> PyResult<Vec<u32>> {
+            let a_arr = a_inner.as_array();
+            let b_arr = b_inner.as_array();
+
+            if a_arr.len() != b_arr.len() {
+                return Err(PyValueError::new_err(
+                    "`a` and `b` don't contain the same number of items.",
+                ));
+            }
+
+            let mut output: Vec<u32> = Vec::with_capacity(a_arr.len());
+            for (a_item, b_item) in a_arr.iter().zip(b_arr.iter()) {
+                output.push(a_item.to_bits() | b_item.to_bits());
+            }
+
+            Ok(output)
+        })
+        .collect::<PyResult<Vec<Vec<u32>>>>()?
+        .into_iter()
+        .flatten()
+        .collect::<Vec<u32>>();
+
+    Ok(output)
 }
