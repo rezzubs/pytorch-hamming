4d19cd90b85a1782d2e358fd5258366103c6ecf2
Marten Roots <rezzubs@proton.me>
2025-10-09 18:09:53 +0300

    feat(NonUniformSequence): Implement ByteChunkedBitBuffer

--- hamming-core/src/wrapper/non_uniform.rs
+++ hamming-core/src/wrapper/non_uniform.rs
@@ -14,10 +14,30 @@ where
     T: BitBuffer,
 {
     /// Return a pair of the index of the item + the index of the bit inside the item.
-    fn inner_index(&self, index: usize) -> (usize, usize) {
+    fn inner_bit_index(&self, index: usize) -> (usize, usize) {
         let mut start_of_current = 0;
         for (i, buffer) in self.0.into_iter().enumerate() {
             let start_of_next = start_of_current + buffer.num_bits();
+            if index < start_of_next {
+                return (i, index - start_of_current);
+            }
+            start_of_current = start_of_next
+        }
+
+        panic!("out of bounds");
+    }
+}
+
+impl<I, T> NonUniformSequence<I>
+where
+    for<'a> &'a I: IntoIterator<Item = &'a T>,
+    T: ByteChunkedBitBuffer,
+{
+    /// Return a pair of the index of the item + the index of the byte inside the item.
+    fn inner_byte_index(&self, index: usize) -> (usize, usize) {
+        let mut start_of_current = 0;
+        for (i, buffer) in self.0.into_iter().enumerate() {
+            let start_of_next = start_of_current + buffer.num_bytes();
             if index < start_of_next {
                 return (i, index - start_of_current);
             }
@@ -39,23 +59,44 @@ where
     }
 
     fn set_1(&mut self, bit_index: usize) {
-        let (outer, inner) = self.inner_index(bit_index);
+        let (outer, inner) = self.inner_bit_index(bit_index);
         self.0[outer].set_1(inner);
     }
 
     fn set_0(&mut self, bit_index: usize) {
-        let (outer, inner) = self.inner_index(bit_index);
+        let (outer, inner) = self.inner_bit_index(bit_index);
         self.0[outer].set_0(inner);
     }
 
     fn is_1(&self, bit_index: usize) -> bool {
-        let (outer, inner) = self.inner_index(bit_index);
+        let (outer, inner) = self.inner_bit_index(bit_index);
         self.0[outer].is_1(inner)
     }
 
     fn flip_bit(&mut self, bit_index: usize) {
-        let (outer, inner) = self.inner_index(bit_index);
+        let (outer, inner) = self.inner_bit_index(bit_index);
         self.0[outer].flip_bit(inner)
+    }
+}
+
+impl<I, T> ByteChunkedBitBuffer for NonUniformSequence<I>
+where
+    for<'a> &'a I: IntoIterator<Item = &'a T>,
+    I: std::ops::IndexMut<usize, Output = T>,
+    T: ByteChunkedBitBuffer,
+{
+    fn num_bytes(&self) -> usize {
+        self.0.into_iter().map(|x| x.num_bytes()).sum()
+    }
+
+    fn get_byte(&self, n: usize) -> u8 {
+        let (outer, inner) = self.inner_byte_index(n);
+        self.0[outer].get_byte(inner)
+    }
+
+    fn set_byte(&mut self, n: usize, value: u8) {
+        let (outer, inner) = self.inner_byte_index(n);
+        self.0[outer].set_byte(inner, value)
     }
 }
 
@@ -118,6 +159,47 @@ mod tests {
         let copied = a_true.copy_into(0, &mut b);
         assert_eq!(copied, a_true.num_bits());
         assert_eq!(copied, b_true.num_bits());
+
+        assert_eq!(b, b_true);
+
+        let mut a: NonUniformSequence<Vec<Vec<u8>>> = NonUniformSequence(vec![
+            vec![0],
+            vec![0, 0, 0],
+            vec![0, 0],
+            vec![0],
+            vec![],
+            vec![0],
+        ]);
+
+        let copied = b_true.copy_into(0, &mut a);
+        assert_eq!(copied, a_true.num_bits());
+        assert_eq!(copied, b_true.num_bits());
+
+        assert_eq!(a, a_true);
+    }
+
+    #[test]
+    fn copy_into_chunked() {
+        let a_true: NonUniformSequence<Vec<Vec<u8>>> = NonUniformSequence(vec![
+            vec![123],
+            vec![13, 255, 8],
+            vec![0, 1],
+            vec![255],
+            vec![],
+            vec![0],
+        ]);
+        let b_true = [
+            u16::from_le_bytes([123, 13]),
+            u16::from_le_bytes([255, 8]),
+            u16::from_le_bytes([0, 1]),
+            u16::from_le_bytes([255, 0]),
+        ];
+
+        let mut b: Vec<u16> = vec![0; 4];
+
+        let copied = a_true.copy_into_chunked(0, &mut b);
+        assert_eq!(copied, a_true.num_bytes());
+        assert_eq!(copied, b_true.num_bytes());
 
         assert_eq!(b, b_true);
 
